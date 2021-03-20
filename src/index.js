79ed03bbc86c1273f34aeb44dbeecc4232e05feb
Nathan Czachur <31296990+nathanczachur@users.noreply.github.com>
2021-03-20 17:33:38 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -54,19 +54,50 @@ app.get("/flashscore/matches", async (req, res) => {
     })
     const page = await browser.newPage()
     await page.goto("https://www.flashscore.com", {
-      timeout: 10 * 1000,
+      timeout: 20 * 1000,
       waitUntil: "load"
     })
-    if (req.query.timeout) {
-      await page.waitForTimeout(parseInt(req.query.timeout, 10))
-    }
-    await page.click('#live-table > div.tabs > div.calendar > div.calendar__datepicker');
+    
+    const starredLeagues = await page.$$eval('#live-table > section > div > div > div.event__header.top', el => el.innerText);
+    
+    var getPreviousSibling = function (elem, selector) {
+
+      // Get the next sibling element
+      var sibling = elem.previousElementSibling;
+
+      // If there's no selector, return the first sibling
+      if (!selector) return sibling;
+
+      // If the sibling matches our selector, use it
+      // If not, jump to the next sibling and continue the loop
+      while (sibling) {
+        if (sibling.matches(selector)) return sibling;
+        sibling = sibling.previousElementSibling;
+      }
+    };
+
+    const matches = await page
+      .$$eval('#live-table > section > div > div > div.event__match', (els) => {
+        return els.map(el => {
+         return {
+          league: getPreviousSibling(el, '.event__header').innerText || 'undefined',
+          homeTeamName: el.querySelector('.event__participant.event__participant--home').innerText || 'undefined',
+          awayTeamName: el.querySelector('.event__participant.event__participant--away').innerText || 'undefined',
+          fullTimeScore: el.querySelector('.event__scores').innerText || 'undefined',
+          halfTimeScore: el.querySelector('.event__part').innerText || 'undefined'
+        } 
+        })
+      })
+      .filter(match => {
+        return starredLeagues.includes(match.league)
+      })
+    ;
+
     await browser.close()
 
-    let matches = []
     let data = {
       status: 'success',
-      data: matches
+      matches: matches
     }
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(data, null, 3));
